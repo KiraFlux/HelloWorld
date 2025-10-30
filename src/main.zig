712db04b49dc6050a6e56d1f3c88b770aceb00cf
KiraFlux <pret2titio1n@gmail.com>
2025-10-30 11:07:18 +0300

    echo fn

--- src/main.zig
+++ src/main.zig
@@ -2,11 +2,15 @@ const std = @import("std");
 
 const print = std.debug.print;
 
+fn echo(string: []u8) void {
+    std.debug.print("-> '{s}'\n", .{string});
+}
+
 pub fn main() !void {
     var stdin = std.fs.File.stdin();
     var stdin_buffer: [1024]u8 = undefined;
     var stdin_reader = stdin.reader(&stdin_buffer);
 
     const out = try stdin_reader.interface.takeDelimiterExclusive('\n');
-    print("Typed: {s}\n", .{out});
+    echo(out);
 }
